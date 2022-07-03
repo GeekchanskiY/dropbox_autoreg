e17efdeb36918772f930fc7e99d596f1d6e05955
GeekchanskiY <astrovskiy.04@gmail.com>
2022-07-04 00:39:35 +0300

    file write ready

--- main.py
+++ main.py
@@ -14,7 +14,6 @@ digits = list(string.digits)
 special_characters = list("!@#$%^&*()")
 characters = list(string.ascii_letters + string.digits + "!@#$%^&*()")
 
-
 def generate_random_password() -> str:
     """
         Generates random password
@@ -90,6 +89,7 @@ def main(name, surname, email, password) -> bool:
     reg = register(res["code"])
     if reg == "funcaptcha_error":
         solver.report(res["captchaId"], False)
+        m = main(name, surname, email, password)
     else:
         solver.report(res["captchaId"], True)
     if reg == "email_error":
@@ -100,7 +100,6 @@ def main(name, surname, email, password) -> bool:
 if __name__ == '__main__':
     with open("orders.txt", "r", encoding="utf-8") as f:
         file_data: str = f.read()
-    line: str
     output_data: list[dict] = []
     for line in file_data.split("\n"):
         line_data: list[str] = line.split(":")
@@ -108,6 +107,7 @@ if __name__ == '__main__':
         user_name: str = line_data[1]
         user_surname: str = line_data[2]
         user_password: str = generate_random_password()
+        print(f"Registering {user_name}")
         registered = main(email=user_email, name=user_name, surname=user_surname, password=user_password)
         if registered:
             output_data.append(
@@ -118,3 +118,13 @@ if __name__ == '__main__':
                     "user_email": user_email
                 }
             )
+            print("registered")
+    result_str: str = ""
+    print("Writing data")
+    for line in output_data:
+        result_str += line["user_name"] + ":" + line["user_surname"] + ":" + line["user_password"] + ":" +\
+                      line["user_email"] + "\n"
+    with open("registered.txt", "w", encoding="utf-8") as f:
+        f.write(result_str)
+    print("Finished!")
+    exit()
