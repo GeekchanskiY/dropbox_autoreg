edfc093133aa1a501d2c3d2af1683e3403d04a94
GeekchanskiY <astrovskiy.04@gmail.com>
2022-07-03 23:26:30 +0300

    password generation ready

--- main.py
+++ main.py
@@ -1,10 +1,53 @@
 import requests
 from twocaptcha import TwoCaptcha
 from vars import TWOCAPTCHA_KEY
+import string
+import random
 
 solver = TwoCaptcha(TWOCAPTCHA_KEY)
 
 SITE_KEY: str = "68CECE5D-F360-8653-CA80-3CF99353DDD2"
+
+
+alphabets = list(string.ascii_letters)
+digits = list(string.digits)
+special_characters = list("!@#$%^&*()")
+characters = list(string.ascii_letters + string.digits + "!@#$%^&*()")
+
+
+def generate_random_password() -> str:
+    """
+        Generates random password
+    Return:
+        (str) - password
+    """
+    length = 12
+
+    alphabets_count = 4
+    digits_count = 4
+    special_characters_count = 4
+
+    characters_count = alphabets_count + digits_count + special_characters_count
+
+    password = []
+
+    for i in range(alphabets_count):
+        password.append(random.choice(alphabets))
+
+    for i in range(digits_count):
+        password.append(random.choice(digits))
+
+    for i in range(special_characters_count):
+        password.append(random.choice(special_characters))
+
+    if characters_count < length:
+        random.shuffle(characters)
+        for i in range(length - characters_count):
+            password.append(random.choice(characters))
+
+    random.shuffle(password)
+
+    return " ".join(map(str, password)).replace(" ", "")
 
 
 def register(funcapcha_response: str,) -> str:
@@ -55,4 +98,13 @@ def main() -> bool:
 
 
 if __name__ == '__main__':
-    main()
+    with open("orders.txt", "r", encoding="utf-8") as f:
+        file_data: str = f.read()
+    line: str
+    output_data: list[dict["user_name", "user_surname", "email", "password"]] = []
+    for line in file_data.split("\n"):
+        line_data: list[str] = line.split(":")
+        email: str = line_data[0]
+        user_name: str = line_data[1]
+        user_surname: str = line_data[2]
+        main()
